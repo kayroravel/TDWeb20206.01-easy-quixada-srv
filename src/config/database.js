7c4b707921183642bfd06eca0ad14adbb1417431
kayroravel <kayroravel@hotmail.com>
2026-07-07 19:38:59 -0300

    Ajusta conexão PostgreSQL para DATABASE_URL

--- src/config/database.js
+++ src/config/database.js
@@ -2,12 +2,10 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 5432,
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_NAME || "easyquixada-bd",
-  connectionTimeoutMillis: 5000,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool
